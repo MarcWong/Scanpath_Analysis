627d739afce88eaeb6445f578ea3f51ecbe8e3af
Yao Wang <yao.wang@vis.uni-stuttgart.de>
2024-09-02 21:49:43 +0200

    feat: merge into one file

--- scanpath_metrics.py
+++ scanpath_metrics.py
@@ -7,152 +7,93 @@ Created on 20240830
 @output : the final avg. Metrics
 """
 
-import pandas as pd
-import numpy as np
 import os
 from glob import glob
 import argparse
-from PIL import Image
+import numpy as np
+import pandas as pd
 pd.options.mode.chained_assignment = None
+from PIL import Image
 from tqdm import trange
 from pathlib import Path
-from utils.metrics import DTW, scaled_TDE, nw_matching
-from tqdm import trange
+from utils.metrics import DTW, nw_matching
 from utils.utils import get_gt_strings, get_gt_files
+
+DTWs_A = []
+DTWs_B = []
+DTWs_C = []
     
+def calc_metrics(gt_files, imgname, im, pred_path):
+    width, height = im.size # original image size
 
-def evaluate_deepgaze(data_path:str, img_path: str, pred_path:str):
+    if 'UMSS' in pred_path:
+        predCsv = os.path.join(pred_path, f'{imgname}.csv') # all of the predictions are in this file
+        if not os.path.exists(predCsv): return
+        df_pred = pd.read_csv(predCsv)
+        df_pred.columns = ['user', 'index', 'time', 'x', 'y']
+        # Normalization required
+        df_pred['x'] = df_pred['x'] * width / 640
+        df_pred['y'] = df_pred['y'] * height / 480
+    elif 'deepgaze' in pred_path:
+        predNpy = np.load(os.path.join(pred_path, f'{imgname}.npy'))
+        predNpy[:,:,0] = predNpy[:,:,0] * width / 8192
+        predNpy[:,:,1] = predNpy[:,:,1] * height / 4096
+    elif 'ours' in pred_path:
+        # TODO: add the code for our model
+        predCsv = os.path.join(pred_path, f'{imgname}.csv') # all of the predictions are in this file
+        if not os.path.exists(predCsv): return
+        df_pred = pd.read_csv(predCsv)
+        df_pred.columns = ['user', 'index', 'time', 'x', 'y']
+
+    # GT
+    if not gt_files: return
+    for t, gt_files_type in enumerate(gt_files):
+        DTWs_type = []
+
+        for gg in range(len(gt_files_type)):
+            df_gt = pd.read_csv(gt_files_type[gg], sep='\t').dropna()
+            if df_gt.empty: continue
+            df_gt.columns = ['time', 'index', 'x', 'y']
+            ########## 2D Metrics
+            df_gt = df_gt.drop(['index','time'],axis=1).to_numpy()
+            ########## 3D Metrics
+            #df_gt = df_gt.drop(['index'],axis=1)
+
+            for pp in range(1, len(gt_files_type) + 1):
+                if 'UMSS' in pred_path or 'ours' in pred_path:
+                    df_predI = df_pred[df_pred['user'] == pp]
+                    ########## 2D Metrics
+                    df_predI = df_predI.drop(['user', 'index', 'time'] ,axis=1)
+                elif 'deepgaze' in pred_path:
+                    df_predI = predNpy[pp-1]
+                id_dtw = DTW(df_predI, df_gt)
+                DTWs_type.append(id_dtw)
+        if t == 0:
+            DTWs_A.extend(DTWs_type)
+        elif t == 1:
+            DTWs_B.extend(DTWs_type)
+        elif t == 2:
+            DTWs_C.extend(DTWs_type)
+
+def evaluate_DTW(data_path:str, img_path: str, pred_path:str):
+    strpath = os.path.join(pred_path, 'str')
     gt_path = os.path.join(data_path, 'fixations')
     visualisations = glob(os.path.join(img_path,'*.png'))
-    DTWs_a = []
-    DTWs_b = []
-    DTWs_c = []
-    STDEs_a = []
-    STDEs_b = []
-    STDEs_c = []
     for idx in trange(len(visualisations)):
         imgfullpath = visualisations[idx]
         with Image.open(imgfullpath) as im:
-            width, height = im.size # original image size
             imgname = imgfullpath.split('/')[-1]
             imgname = imgname.strip('.png')
-            predNpy = np.load(os.path.join(pred_path, f'{imgname}.npy'))
-            predNpy[:,:,0] = predNpy[:,:,0] * width / 8192
-            predNpy[:,:,1] = predNpy[:,:,1] * height / 4096
-
-            # GT
             gt_files = get_gt_files(f'{imgname}.png', gt_path)
-            if not gt_files: continue
-            for t, gt_files_type in enumerate(gt_files):
-                DTWs_type = []
-                STDEs_type = []
+            calc_metrics(gt_files, imgname, im, pred_path)
+    return DTWs_A, DTWs_B, DTWs_C
 
-                for gg in range(len(gt_files_type)):
-                    df_gt = pd.read_csv(gt_files_type[gg], sep='\t').dropna()
-                    if df_gt.empty: continue
-                    df_gt.columns = ['time', 'index', 'x', 'y']
-                    ########## 2D Metrics
-                    df_gt = df_gt.drop(['index','time'],axis=1)
-                    ########## 3D Metrics
-                    #df_gt = df_gt.drop(['index'],axis=1)
-
-                    for pp in range(1, len(gt_files_type) + 1):
-                        id_dtw = DTW(predNpy[pp-1], df_gt.to_numpy())
-                        id_stde = scaled_TDE(predNpy[pp-1],df_gt.to_numpy(), im)
-
-                        DTWs_type.append(id_dtw)
-                        STDEs_type.append(id_stde)
-                if t == 0:
-                    DTWs_a.extend(DTWs_type)
-                    STDEs_a.extend(STDEs_type)
-                elif t == 1:
-                    DTWs_b.extend(DTWs_type)
-                    STDEs_b.extend(STDEs_type)
-                elif t == 2:
-                    DTWs_c.extend(DTWs_type)
-                    STDEs_c.extend(STDEs_type)
-
-    df = pd.DataFrame(list(zip(DTWs_a,DTWs_b,DTWs_c, STDEs_a, STDEs_b, STDEs_c)), columns = ['DTW_a', 'DTW_b', 'DTW_c', 'STDE_a', 'STDE_b', 'STDE_c'])
-    Path.mkdir(Path('evaluation'), exist_ok=True)
-    df.to_csv('evaluation/deepgaze.csv', index=False)
-
-    print(predpath, np.round(np.mean(STDEs_a), 3), np.round(np.std(STDEs_a), 3))
-    print(predpath, np.round(np.mean(STDEs_b), 3), np.round(np.std(STDEs_b), 3))
-    print(predpath, np.round(np.mean(STDEs_c), 3), np.round(np.std(STDEs_c), 3))
-
-def evaluate_UMSS(data_path:str, img_path: str, pred_path:str):
-    gt_path = os.path.join(data_path, 'fixations')
-    visualisations = glob(os.path.join(img_path,'*.png'))
-    DTWs_a = []
-    DTWs_b = []
-    DTWs_c = []
-    STDEs_a = []
-    STDEs_b = []
-    STDEs_c = []
-    for idx in trange(len(visualisations)):
-        imgfullpath = visualisations[idx]
-        with Image.open(imgfullpath) as im:
-            width, height = im.size # original image size
-            imgname = imgfullpath.split('/')[-1]
-            imgname = imgname.strip('.png')
-            predCsv = os.path.join(pred_path, f'{imgname}.csv') # all of the predictions are in this file
-            if not os.path.exists(predCsv): continue
-            df_pred = pd.read_csv(predCsv)
-            df_pred.columns = ['user', 'index', 'time', 'x', 'y']
-
-            # GT
-            gt_files = get_gt_files(f'{imgname}.png', gt_path)
-            if not gt_files: continue
-            for t, gt_files_type in enumerate(gt_files):
-                DTWs_type = []
-                STDEs_type = []
-
-                for gg in range(len(gt_files_type)):
-                    df_gt = pd.read_csv(gt_files_type[gg], sep='\t').dropna()
-                    if df_gt.empty: continue
-                    df_gt.columns = ['time', 'index', 'x', 'y']
-                    ########## 2D Metrics
-                    df_gt = df_gt.drop(['index','time'],axis=1)
-                    ########## 3D Metrics
-                    #df_gt = df_gt.drop(['index'],axis=1)
-
-                    for pp in range(1, len(gt_files_type) + 1):
-                        df_predI = df_pred[df_pred['user'] == pp]
-                        ########## 2D Metrics
-                        df_predI = df_predI.drop(['user', 'index', 'time'] ,axis=1)
-
-                        # Normalization required
-                        df_predI['x'] = df_predI['x'] * width / 640
-                        df_predI['y'] = df_predI['y'] * height / 480
-
-                        id_dtw = DTW(df_predI, df_gt.to_numpy())
-                        id_stde = scaled_TDE(df_predI,df_gt.to_numpy(), im)
-
-                        DTWs_type.append(id_dtw)
-                        STDEs_type.append(id_stde)
-                if t == 0:
-                    DTWs_a.extend(DTWs_type)
-                    STDEs_a.extend(STDEs_type)
-                elif t == 1:
-                    DTWs_b.extend(DTWs_type)
-                    STDEs_b.extend(STDEs_type)
-                elif t == 2:
-                    DTWs_c.extend(DTWs_type)
-                    STDEs_c.extend(STDEs_type)
-
-    df = pd.DataFrame(list(zip(DTWs_a,DTWs_b,DTWs_c, STDEs_a, STDEs_b, STDEs_c)), columns = ['DTW_a', 'DTW_b', 'DTW_c', 'STDE_a', 'STDE_b', 'STDE_c'])
-    Path.mkdir(Path('evaluation'), exist_ok=True)
-    df.to_csv('evaluation/UMSS.csv', index=False)
-
-    print(predpath, np.round(np.mean(STDEs_a), 3), np.round(np.std(STDEs_a), 3))
-    print(predpath, np.round(np.mean(STDEs_b), 3), np.round(np.std(STDEs_b), 3))
-    print(predpath, np.round(np.mean(STDEs_c), 3), np.round(np.std(STDEs_c), 3))
-
-def evaluate_SS(imgpath: str, strpath: str, gtpath: str, is_simplified=False):
-    visualisations = glob(imgpath+'/*.png')
+def evaluate_SS(imgpath: str, predpath: str, gtpath: str, is_simplified=False):
     SS_A = []
     SS_B = []
     SS_C = []
+    strpath = os.path.join(predpath, 'str')
+    visualisations = glob(imgpath+'/*.png')
     for idx in trange(len(visualisations)):
         imgfullpath = visualisations[idx]
         imgname = imgfullpath.split('/')[-1]
@@ -174,11 +115,7 @@ def evaluate_SS(imgpath: str, strpath: str, gtpath: str, is_simplified=False):
                 SS_B.extend(SS_type)
             elif t == 2:
                 SS_C.extend(SS_type)
-
-    df = pd.DataFrame(list(zip(SS_A, SS_B, SS_C)))
-    df.columns = ['SS_A', 'SS_B', 'SS_C']
-    df.to_csv(f'evaluation/{strpath.split("/")[-2]}_ss.csv', index=False)
-    print(np.mean(np.array(SS_A)), np.mean(np.array(SS_B)), np.mean(np.array(SS_C)))
+    return SS_A, SS_B, SS_C
 
 
 if __name__ == '__main__':
@@ -190,11 +127,17 @@ if __name__ == '__main__':
 
     imgpath = os.path.join('evaluation', 'images')
     predpath = os.path.join('evaluation', 'scanpaths', args['pred_name'])
-    if args['pred_name'] == 'UMSS':
-        evaluate_UMSS(args['data_path'],imgpath, predpath)
-    elif args['pred_name'] == 'deepgaze':
-        evaluate_deepgaze(args['data_path'],imgpath, predpath)
-
     gtpath = os.path.join('taskvis_analysis', 'fixationsByVis')
-    strpath = os.path.join('evaluation', 'scanpaths', args['pred_name'], 'str')
-    evaluate_SS(imgpath, strpath, gtpath, is_simplified=args['is_simplified_ss'])
+    print('evaluating Dynamic Time Warpping')
+    DTWs_a, DTWs_b, DTWs_c = evaluate_DTW(args['data_path'],imgpath, predpath)
+    print('evaluating Sequence Score')
+    SS_A, SS_B, SS_C = evaluate_SS(imgpath, predpath, gtpath, is_simplified=args['is_simplified_ss'])
+
+    print(np.mean(np.array(SS_A)), np.mean(np.array(SS_B)), np.mean(np.array(SS_C)))
+    print(predpath, np.round(np.mean(DTWs_a), 3), np.round(np.std(DTWs_a), 3))
+    print(predpath, np.round(np.mean(DTWs_b), 3), np.round(np.std(DTWs_b), 3))
+    print(predpath, np.round(np.mean(DTWs_c), 3), np.round(np.std(DTWs_c), 3))
+
+    df = pd.DataFrame(list(zip(DTWs_a,DTWs_b,DTWs_c, SS_A, SS_B, SS_C)), columns = ['DTW_A', 'DTW_B', 'DTW_C', 'SS_A', 'SS_B', 'SS_C'])
+    Path.mkdir(Path('evaluation'), exist_ok=True)
+    df.to_csv(f'evaluation/{args["pred_name"]}.csv', index=False)
--- scanpath_metrics.sh
+++ scanpath_metrics.sh
@@ -1,1 +1,3 @@
 python scanpath_metrics.py --pred_name deepgaze
+# python scanpath_metrics.py --pred_name UMSS
+# python scanpath_metrics.py --pred_name ours
